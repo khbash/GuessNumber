298798201434c56d01b24b0c10fe1542e918ed9e
Khrystyna Bash <khrystynabash09@gmail.com>
2025-11-25 11:30:47 +0200

    new code

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+while True:
+    secret_num = random.randint(1,50)
+    attempts = 0
+
+    while True:
+        user_input = input("Введи свій варіант: ")
+        if user_input.isdigit():
+            guess = int(user_input)
+            attempts += 1
+
+            if guess < secret_num:
+                print("Моє число більше")
+
+            elif guess > secret_num:
+                print("Моє число меньше")
+            else:
+                print("Вітаю! Ти вгадав число")
+else:
+    print("Помилка! Тільки цілі числа")
